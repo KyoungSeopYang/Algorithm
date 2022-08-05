acc54735e94564e59250ec00155d5ee1191b4a13
JerryCoding <yangksks@gmail.com>
2022-08-05 23:28:28 +0900

    0805

--- 백준/day0803/Main_G4_14499_주사위굴리기.java
+++ 백준/day0803/Main_G4_14499_주사위굴리기.java
@@ -7,84 +7,87 @@ import java.util.StringTokenizer;
 
 public class Main_G4_14499_주사위굴리기 {
 
+//  1
+	// 234
+	//  5
+	//  6
 	static int[] dice = new int[7];
-	static int[] top = { 1, 1 };
-	static int[] dx = { 0, 0, -1, 1 };
-	static int[] dy = { 1, -1, 0, 0 };
-
-	public static void main(String[] args) throws IOException {
+	static int n,m,x,y;
+	static int[][] map;
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken()); // 세로
-		int M = Integer.parseInt(st.nextToken()); // 가로
-		int x = Integer.parseInt(st.nextToken()); // x좌표
-		int y = Integer.parseInt(st.nextToken()); // y좌표
-		int K = Integer.parseInt(st.nextToken()); // 명령의 개수
-
-		int[][] map = new int[N][M];
-
-		for (int i = 0; i < N; i++) {
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		y = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		
+		int k = Integer.parseInt(st.nextToken());
+		
+		map = new int[n][m];
+		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
+			for(int j=0; j<m; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
+		
 		st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < K; i++) {
-			int dir = Integer.parseInt(st.nextToken());
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-
-			if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-				continue;
-
-			changeDice(dir);
-
-			if (map[nx][ny] == 0) {
-				map[nx][ny] = dice[6];
-			} else {
-				dice[6] = map[nx][ny];
-				map[nx][ny] = 0;
-			}
-
-			x = nx;
-			y = ny;
+		for(int i=0; i<k; i++) {
+			int d = Integer.parseInt(st.nextToken());
+			move(d);
 		}
-
-		System.out.println(sb.toString());
-
+		
+	}
+	
+	static void move(int d) {
+		int nx = x + dx[d-1];
+		int ny = y + dy[d-1];
+		if(nx <0 || ny < 0 || nx > m-1 || ny > n-1) return;
+		roll(d, nx, ny);
+		x = nx; y = ny;
+		
 	}
 
-	private static void changeDice(int dir) {
-		int[] temp = dice.clone();
-
-		// 동쪽은 1, 서쪽은 2 , 북쪽은 3, 남쪽은 4
-		if (dir == 1) {
-			dice[1] = temp[4];
-			dice[3] = temp[1];
-			dice[4] = temp[6];
-			dice[6] = temp[3];
-		} else if (dir == 2) {
-			dice[1] = temp[3];
-			dice[3] = temp[6];
-			dice[4] = temp[1];
-			dice[6] = temp[4];
-		} else if (dir == 3) {
-			dice[1] = temp[5];
-			dice[2] = temp[1];
-			dice[5] = temp[6];
-			dice[6] = temp[2];
+	// 1 2 3 4 (동 서 남 북)
+	static void roll(int d, int x, int y) {
+		int tmp = dice[3];
+		switch(d) {
+		case 1:
+			dice[3] = dice[4];
+			dice[4] = dice[6];
+			dice[6] = dice[2];
+			dice[2] = tmp;
+			break;
+		case 2:
+			dice[3] = dice[2];
+			dice[2] = dice[6];
+			dice[6] = dice[4];
+			dice[4] = tmp;
+			break;
+		case 3:
+			dice[3] = dice[5];
+			dice[5] = dice[6];
+			dice[6] = dice[1];
+			dice[1] = tmp;
+			break;
+		case 4:
+			dice[3] = dice[1];
+			dice[1] = dice[6];
+			dice[6] = dice[5];
+			dice[5] = tmp;
+			break;
+		}
+		if(map[y][x] == 0) {
+			map[y][x] = dice[6];
 		} else {
-			dice[1] = temp[2];
-			dice[2] = temp[6];
-			dice[5] = temp[1];
-			dice[6] = temp[5];
+			dice[6] = map[y][x];
+			map[y][x] =0;
 		}
-
+		System.out.println(dice[3]);
+		
 	}
-
 }
