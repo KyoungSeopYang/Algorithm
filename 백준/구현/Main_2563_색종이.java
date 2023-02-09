68019c9b71a1af90021c743c0047533c1ae6388c
양경섭 <yangksks@gmail.com>
2023-02-09 21:24:41 +0900

    0209

--- 백준/구현/Main_2563_색종이.java
+++ 백준/구현/Main_2563_색종이.java
@@ -1,33 +1,36 @@
-package com.ssafy.day0209;
+package day0209;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_2563_색종이 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[][] paper = new int[100][100];
+        int[][] paper = new int[100][100];
 
-		int N = Integer.parseInt(br.readLine());
-		int over = 0;
-		for (int n = 0; n < N; n++) {
-			String[] str = br.readLine().split(" ");
-			int x = Integer.parseInt(str[1]);
-			int y = Integer.parseInt(str[0]);
+        int N = Integer.parseInt(br.readLine());
+        int over = 0;
 
-			for (int i = x; i < x + 10; i++) {
-				for (int j = y; j < y + 10; j++) {
-					if (paper[i][j] == 1) {
-						over++;
-					} else
-						paper[i][j]++;
-				}
-			}
-		}
-		System.out.println(N * 100 - over);
+        for (int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
 
-	}
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (paper[x + i][y + j] == 1) {
+                        over++;
+                    } else
+                        paper[x + i][y + j] = 1;
+                }
+            }
+        }
+
+        System.out.println(N * 100 - over);
+    }
 }
