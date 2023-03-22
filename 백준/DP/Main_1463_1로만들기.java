ce8de3dc83ebc2d9beebad4bc26e5c32214eb59c
양경섭 <yangksks@gmail.com>
2023-03-22 20:34:34 +0900

    0322

---
+++ 백준/DP/Main_1463_1로만들기.java
@@ -0,0 +1,30 @@
+package day0322;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1463_1로만들기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+
+            dp[i] = dp[i - 1] + 1;
+            if (i % 3 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            if (i % 2 == 0)
+                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+
+
+        }
+
+        System.out.println(dp[N]);
+    }
+}
