9a45e006b2a7c1139033c9543d76644e94436669
양경섭 <yangksks@gmail.com>
2023-04-01 19:37:50 +0900

    0401

---
+++ 그리디/Main_ATM_11399.java
@@ -0,0 +1,32 @@
+package day0401;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_ATM_11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] list = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int totalTime = 0;
+        int time = 0;
+
+        Arrays.sort(list);
+
+        for (int t : list) {
+            time += t;
+            totalTime += time;
+        }
+
+        System.out.println(totalTime);
+    }
+}
