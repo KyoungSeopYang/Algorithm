9dc39277aea6ea1a80d3aab27471bcf8ffdce32a
양경섭 <yangksks@gmail.com>
2023-05-03 21:52:31 +0900

    0503

---
+++ 그리디/Main_1080_행렬.java
@@ -0,0 +1,59 @@
+package day0503;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1080_행렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+        int[][] B = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                A[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                B[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = 0; j < M - 2; j++) {
+                if (A[i][j] != B[i][j]) {
+                    for (int k = i; k < i + 3; k++) {
+                        for (int l = j; l < j + 3; l++) {
+                            A[k][l] = A[k][l] == 0 ? 1 : 0;
+                        }
+                    }
+                    cnt++;
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (A[i][j] != B[i][j]) {
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+}
