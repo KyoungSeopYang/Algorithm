41102bcd74d9140864480e33349fa96ee0f33e28
양경섭 <yangksks@gmail.com>
2023-05-21 19:41:05 +0900

    0521

---
+++ DP/Main_1010_다리놓기.java
@@ -0,0 +1,37 @@
+package day0521;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1010_다리놓기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[30][30];
+
+        for (int i = 0; i < 30; i++) {
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+        }
+
+        for (int i = 2; i < 30; i++) {
+            for (int j = 1; j < 30; j++) {
+                // 1번 성질
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            sb.append(dp[M][N]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
